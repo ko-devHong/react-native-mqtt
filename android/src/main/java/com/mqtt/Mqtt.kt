16bac57f273da1d7f56c19151c7b0721299e2785
ko-devHong <dea972@naver.com>
2024-04-22 16:27:25 +0900

    chore: android logic success

--- android/src/main/java/com/mqtt/Mqtt.kt
+++ android/src/main/java/com/mqtt/Mqtt.kt
@@ -2,11 +2,14 @@ package com.mqtt
 
 import android.util.Base64;
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.Callback
 import com.facebook.react.modules.core.RCTNativeAppEventEmitter
+
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.IMqttToken
@@ -16,13 +19,26 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import java.util.concurrent.atomic.AtomicReference
-import com.facebook.react.bridge.Callback
-import com.heroku.sdk.EnvKeyStore;
 import org.eclipse.paho.client.mqttv3.IMqttAsyncClient
-import java.security.cert.Certificate;
 
-class Mqtt(reactContext: ReactApplicationContext, originId:String) {
+import java.util.concurrent.atomic.AtomicReference
+import com.heroku.sdk.EnvKeyStore
+
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import java.security.KeyStore
+import java.security.SecureRandom
+
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.KeyManager
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.TrustManagerFactory
+
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
+class Mqtt(reactContext: ReactApplicationContext, originId: String) {
 
   init {
     _reactContext = reactContext
@@ -32,65 +48,65 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   fun connect(host: String?, options: ReadableMap, callback: Callback) {
 
     connectCallback.set(callback)
-    try  {
+    try {
       client.set(MqttAsyncClient(host, clientId, MemoryPersistence()))
       val connOpts = MqttConnectOptions()
       connOpts.isCleanSession = !options.hasKey("cleanSession") || options.getBoolean("cleanSession")
-      connOpts.setKeepAliveInterval(if (options.hasKey("keepAliveInterval"))options.getInt("keepAliveInterval") else 60)
-      connOpts.setConnectionTimeout(if (options.hasKey("timeout"))options.getInt("timeout") else 10)
+      connOpts.setKeepAliveInterval(if (options.hasKey("keepAliveInterval")) options.getInt("keepAliveInterval") else 60)
+      connOpts.setConnectionTimeout(if (options.hasKey("timeout")) options.getInt("timeout") else 10)
       connOpts.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1_1)
-      connOpts.setMaxInflight(if (options.hasKey("maxInFlightMessages"))options.getInt("maxInFlightMessages") else 10)
+      connOpts.setMaxInflight(if (options.hasKey("maxInFlightMessages")) options.getInt("maxInFlightMessages") else 10)
       connOpts.isAutomaticReconnect = options.hasKey("autoReconnect") && options.getBoolean("autoReconnect")
       if (options.hasKey("username")) {
         connOpts.setUserName(options.getString("username"))
       }
-      if(options.hasKey("password")) {
+      if (options.hasKey("password")) {
         connOpts.password = options.getString("password")!!.toCharArray()
       }
       if (options.hasKey("tls")) {
         val tlsOptions: ReadableMap? = options.getMap("tls")
-        val ca: String? = if (tlsOptions!!.hasKey("caDer")) tlsOptions!!.getString("caDer") else null
-        val cert: String? = if (tlsOptions!!.hasKey("cert")) tlsOptions!!.getString("cert") else null
-        val key: String? = if (tlsOptions!!.hasKey("key")) tlsOptions!!.getString("key") else null
-        var keyManagers: Array<javax.net.ssl.KeyManager?>? = null
-        var trustManagers: Array<javax.net.ssl.TrustManager?>? = null
+        val ca: String? = if (tlsOptions!!.hasKey("caDer")) tlsOptions.getString("caDer") else null
+        val cert: String? = if (tlsOptions.hasKey("cert")) tlsOptions.getString("cert") else null
+        val key: String? = if (tlsOptions.hasKey("key")) tlsOptions.getString("key") else null
+        var keyManagers: Array<KeyManager?>? = null
+        var trustManagers: Array<TrustManager?>? = null
         if (cert != null && key != null) {
-          val keyManagerFactory: javax.net.ssl.KeyManagerFactory = javax.net.ssl.KeyManagerFactory.getInstance("PKIX")
-          val keyStore: java.security.KeyStore = EnvKeyStore.createFromPEMStrings(key, cert, "").keyStore()
+          val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("PKIX")
+          val keyStore: KeyStore = EnvKeyStore.createFromPEMStrings(key, cert, "").keyStore()
           keyManagerFactory.init(keyStore, "".toCharArray())
           keyManagers = keyManagerFactory.keyManagers
         }
         if (ca != null) {
-          val cf: java.security.cert.CertificateFactory = java.security.cert.CertificateFactory.getInstance("X.509")
-          val caInput: java.io.InputStream = java.io.ByteArrayInputStream(Base64.decode(ca, Base64.DEFAULT))
+          val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+          val caInput: InputStream = ByteArrayInputStream(Base64.decode(ca, Base64.DEFAULT))
           val caCert: Certificate = cf.generateCertificate(caInput)
           caInput.close()
-          val keyStore: java.security.KeyStore = java.security.KeyStore.getInstance(java.security.KeyStore.getDefaultType())
+          val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
           keyStore.load(null, null)
           keyStore.setCertificateEntry("ca", caCert)
-          val tmf: javax.net.ssl.TrustManagerFactory = javax.net.ssl.TrustManagerFactory.getInstance(javax.net.ssl.TrustManagerFactory.getDefaultAlgorithm())
+          val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
           tmf.init(keyStore)
           trustManagers = tmf.trustManagers
         }
         if (keyManagers != null || trustManagers != null) {
-          val sslContext: javax.net.ssl.SSLContext = javax.net.ssl.SSLContext.getInstance("TLS")
-          sslContext.init(keyManagers, trustManagers, java.security.SecureRandom())
-          connOpts.setSocketFactory(sslContext.socketFactory)
+          val sslContext: SSLContext = SSLContext.getInstance("TLS")
+          sslContext.init(keyManagers, trustManagers, SecureRandom())
+          connOpts.socketFactory = sslContext.socketFactory
         }
       }
       client.get().setCallback(MqttEventCallback())
       client.get().connect(connOpts, _reactContext, ConnectMqttActionListener())
-    }catch (ex: java.lang.Exception) {
+    } catch (ex: java.lang.Exception) {
       callback.invoke(ex.message)
       ex.printStackTrace()
     }
   }
 
   fun subscribe(topic: String, qos: Int) {
-    try  {
+    try {
       client.get().subscribe(topic, qos, null, SubscribeMqttActionListener())
-    }catch (ex: java.lang.Exception) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+    } catch (ex: java.lang.Exception) {
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error subscribing")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -98,14 +114,14 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
   fun unsubscribe(topicList: ReadableArray) {
-    try  {
+    try {
       val topic: Array<String?> = arrayOfNulls<String>(topicList.size())
-      for (x in 0 until topicList.size())  {
+      for (x in 0 until topicList.size()) {
         topic[x] = topicList.getString(x)
       }
       client.get().unsubscribe(topic, null, UnsubscribeMqttActionListener())
-    }catch (ex: java.lang.Exception) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+    } catch (ex: java.lang.Exception) {
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error unsubscribing")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -116,10 +132,10 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
     val message: MqttMessage = MqttMessage(Base64.decode(base64Payload, Base64.DEFAULT))
     message.setQos(qos)
     message.setRetained(retained)
-    try  {
+    try {
       client.get().publish(topic, message)
-    }catch (ex: MqttException) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+    } catch (ex: MqttException) {
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error publishing message")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -127,10 +143,10 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
   fun disconnect() {
-    try  {
+    try {
       client.get().disconnect(null, DisconnectMqttActionListener())
-    }catch (ex: MqttException) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+    } catch (ex: MqttException) {
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error disconnecting")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -138,10 +154,10 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
   fun close() {
-    try  {
+    try {
       client.get().close()
-    }catch (ex: MqttException) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+    } catch (ex: MqttException) {
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error closing")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -149,33 +165,34 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
 
-  private class MqttEventCallback () : MqttCallbackExtended {
+  private class MqttEventCallback() : MqttCallbackExtended {
     override fun connectionLost(cause: Throwable) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("cause", cause.message)
       sendEvent(DISCONNECT_EVENT, params)
     }
+
     override fun messageArrived(topic: String, message: MqttMessage) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("topic", topic)
       params.putString("message", Base64.encodeToString(message.getPayload(), Base64.DEFAULT))
       sendEvent(MESSAGE_EVENT, params)
     }
+
     override fun deliveryComplete(token: IMqttDeliveryToken) {}
     override fun connectComplete(reconnect: Boolean, serverURI: String) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putBoolean("reconnect", reconnect)
-      params.putString("serverURI",serverURI)
       sendEvent(CONNECT_EVENT, params)
     }
   }
 
 
-
-  private class ConnectMqttActionListener () : IMqttActionListener {
+  private class ConnectMqttActionListener() : IMqttActionListener {
     override fun onSuccess(asyncActionToken: IMqttToken) {
       connectCallback.get().invoke()
     }
+
     override fun onFailure(asyncActionToken: IMqttToken, ex: Throwable) {
       connectCallback.get().invoke(ex.message)
       ex.printStackTrace()
@@ -183,15 +200,15 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
 
-
-  private class DisconnectMqttActionListener () : IMqttActionListener {
+  private class DisconnectMqttActionListener() : IMqttActionListener {
     override fun onSuccess(asyncActionToken: IMqttToken) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("cause", "User disconnected")
       sendEvent(DISCONNECT_EVENT, params)
     }
+
     override fun onFailure(asyncActionToken: IMqttToken, ex: Throwable) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error connecting")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -199,11 +216,10 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
 
-
-  private class SubscribeMqttActionListener () : IMqttActionListener {
+  private class SubscribeMqttActionListener() : IMqttActionListener {
     override fun onSuccess(asyncActionToken: IMqttToken) {}
     override fun onFailure(asyncActionToken: IMqttToken, ex: Throwable) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error subscribing")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -211,11 +227,10 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
   }
 
 
-
-  private class UnsubscribeMqttActionListener () : IMqttActionListener {
+  private class UnsubscribeMqttActionListener() : IMqttActionListener {
     override fun onSuccess(asyncActionToken: IMqttToken) {}
     override fun onFailure(asyncActionToken: IMqttToken, ex: Throwable) {
-      val params: WritableMap = com.facebook.react.bridge.Arguments.createMap()
+      val params: WritableMap = Arguments.createMap()
       params.putString("message", "Error unsubscribing")
       params.putString("error", ex.message)
       sendEvent(ERROR_EVENT, params)
@@ -237,8 +252,7 @@ class Mqtt(reactContext: ReactApplicationContext, originId:String) {
 
     fun sendEvent(eventName: String, params: WritableMap) {
       params.putString("id", clientId)
-      _reactContext?.getJSModule(RCTNativeAppEventEmitter::class.java)
-        ?.emit(eventName, params)
+      _reactContext?.getJSModule(RCTNativeAppEventEmitter::class.java)?.emit(eventName, params)
     }
   }
 }
